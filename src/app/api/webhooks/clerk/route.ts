5206e0821acf404c5becb354cff4ba2ed80a3d2e
Pranshu Verma <pranshuverma1601@gmail.com>
2025-08-23 12:21:16 +0530

    add clerk webhook & user upsert/delete

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+
+import { deleteUser, upsertUser } from "@/features/users/db";
+
+export async function POST(req: NextRequest) {
+  try {
+    const event = await verifyWebhook(req);
+
+    switch (event.type) {
+      case "user.created":
+      case "user.updated":
+        const clerkData = event.data;
+        const email = clerkData.email_addresses.find(
+          (email) => email.id === clerkData.primary_email_address_id
+        )?.email_address;
+
+        if (!email)
+          return NextResponse.json(
+            { error: "No primary email found" },
+            { status: 404 }
+          );
+
+        await upsertUser({
+          id: clerkData.id,
+          name: `${clerkData.first_name} ${clerkData.last_name}`,
+          email,
+          imageUrl: clerkData.image_url,
+          createdAt: new Date(clerkData.created_at),
+          updatedAt: new Date(clerkData.updated_at),
+        });
+        break;
+      case "user.deleted":
+        if (!event.data.id)
+          return NextResponse.json(
+            { mesaage: "No userId found" },
+            { status: 404 }
+          );
+
+        await deleteUser(event.data.id);
+        break;
+    }
+  } catch {
+    return NextResponse.json({ error: "Invalid webhook" }, { status: 400 });
+  }
+  return NextResponse.json(
+    { message: "Webhook processed successfully" },
+    { status: 200 }
+  );
+}
--- src/env/server.ts
+++ src/env/server.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
     ARCJET_KEY: z.string().min(1),
     DB_NAME: z.string().min(1),
     DB_USER: z.string().min(1),
