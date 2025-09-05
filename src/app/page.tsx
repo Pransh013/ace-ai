07c11c99b5920b839774448ea88c4c9833082212
Pranshu Verma <pranshuverma1601@gmail.com>
2025-09-05 18:31:09 +0530

    add billing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import { ThemeToggle } from "@/components/ui/theme-toggle";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
-export default function HomePage() {
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { PricingTable } from "@/services/clerk/components/pricing-table";
+
+export default function RootPage() {
   return (
-    <>
-      <UserButton />
-      <SignInButton />
-      <div>Welcome</div>
-      <ThemeToggle />
-    </>
+    <div className="bg-gradient-to-b from-background to-muted/80 h-screen">
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <SignInButton />
+        <UserButton />
+      </div>
+      <PricingTable />
+    </div>
   );
 }
--- src/services/clerk/components/clerk-provider.tsx
+++ src/services/clerk/components/clerk-provider.tsx
@@ -1,9 +1,63 @@
 import { ClerkProvider as MainClerkProvider } from "@clerk/nextjs";
 
+import { buttonVariants } from "@/components/ui/button";
+
 export function ClerkProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <MainClerkProvider>{children}</MainClerkProvider>;
+  return (
+    <MainClerkProvider
+      appearance={{
+        cssLayerName: "vendor",
+        variables: {
+          borderRadius: "var(--radius-md)",
+          colorBackground: "var(--color-background)",
+          colorForeground: "var(--color-foreground)",
+          colorBorder: "var(--color-secondary-foreground)",
+          colorDanger: "var(--color-destructive)",
+          colorPrimary: "var(--color-primary)",
+          colorPrimaryForeground: "var(--color-primary-foreground)",
+          colorInput: "var(--color-input)",
+          colorInputForeground: "var(--color-input-foreground)",
+          colorMuted: "var(--color-muted)",
+          colorMutedForeground: "var(--color-muted-foreground)",
+          colorNeutral: "var(--color-secondary)",
+          colorRing: "var(--color-ring)",
+          colorShadow: "var(--color-shadow-color)",
+          colorSuccess: "var(--color-primary)",
+          colorWarning: "var(--color-warning)",
+          fontFamily: "var(--font-sans)",
+          fontFamilyButtons: "var(--font-sans)",
+        },
+        elements: {
+          pricingTableCard:
+            "custom-pricing-table bg-none bg-[unset] border border-border p-6 my-3",
+          pricingTableCardHeader: "p-0 pb-12",
+          pricingTableCardTitle: "text-xl",
+          pricingTableCardBody:
+            "flex flex-col justify-end bg-none bg-[unset] *:bg-none *:bg-[unset] [&>.cl-pricingTableCardFeatures]:justify-items-end",
+          pricingTableCardDescription: "text-muted-foreground text-sm mb-2",
+          pricingTableCardFeeContainer: "items-baseline gap-0.5",
+          pricingTableCardFee: "text-4xl",
+          pricingTableCardFeePeriodNotice: "hidden",
+          pricingTableCardFeePeriod: "text-base text-muted-foreground",
+          pricingTableCardFeatures: "p-0 border-none",
+          pricingTableCardFeaturesListItem: "[&>svg]:text-primary",
+          pricingTableCardFeaturesListItemTitle: "text-sm",
+          pricingTableCardFooter: "p-0 pt-8 border-none",
+          pricingTableCardFooterButton: buttonVariants(),
+          switchIndicator: "bg-primary",
+          providerIcon__github:
+            " size-4 dark:invert rounded-full overflow-hidden",
+          userButtonPopoverActionButton: "text-primary",
+          avatarImageActionsUpload: "text-primary",
+          tabButton: "text-primary",
+        },
+      }}
+    >
+      {children}
+    </MainClerkProvider>
+  );
 }
